35cfa0ed805de134e57cba1fa6fd0ef1d0942af2
wilfrank84 <wilfrank84@gmail.com>
2019-12-06 12:02:31 -0300

    adding angular in memory web api

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDatabase } from '../in-memory-database';
+
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    BrowserModule
+    BrowserModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
   ],
   exports: [
     // SHARED MODULES
--- src/app/pages/companies/shared/company.model.ts
+++ src/app/pages/companies/shared/company.model.ts
@@ -2,7 +2,9 @@ import { BaseResourceModel } from 'src/app/shared/models/base-resource.model';
 
 export class Company extends BaseResourceModel {
   constructor(
-    public id?: number
+    public id?: number,
+    public name?: string,
+    public description?: string
   ) {
     super();
   }
